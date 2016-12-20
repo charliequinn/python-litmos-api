b3d363cbf27c2a195830a7edaf2d9552b1ea6785
Charlie Quinn <charlesquinn1984@gmail.com>
2016-12-20 13:07:00 +0000

    added more tests around LitmosType

--- tests/test_litmos.py
+++ tests/test_litmos.py
@@ -3,7 +3,7 @@ from unittest.mock import patch, Mock
 
 from nose.tools import raises, assert_true, eq_
 
-from litmos.litmos import Litmos, LitmosAPI, User
+from litmos.litmos import Litmos, LitmosAPI, LitmosType
 
 
 class TestLitmos:
@@ -19,7 +19,7 @@ class TestLitmos:
         litmos = Litmos('app-key-123456', 'app-name-123456')
         user = litmos.User
 
-        eq_(user, User)
+        eq_(user.__name__, 'User')
 
     @raises(AttributeError)
     def test_non_acceptable_types(self):
@@ -119,18 +119,65 @@ class TestLitmosAPI:
         )
 
 
-class TestUser:
-    @patch('litmos.litmos.LitmosAPI')
-    def test_create(self, api_mock):
-        api_mock.create.return_value = {"dd": 3}
-
-        User.create({'UserName': 'paul.smith', 'FirstName': 'Paul'})
-
-        api_mock.create.assert_called_once_with('users', OrderedDict([('Id', ''), ('UserName', 'paul.smith'), ('FirstName', 'Paul'), ('LastName', ''), ('FullName', ''), ('Email', ''), ('AccessLevel', 'Learner'), ('DisableMessages', True), ('Active', True), ('Skype', ''), ('PhoneWork', ''), ('PhoneMobile', ''), ('LastLogin', ''), ('LoginKey', ''), ('IsCustomUsername', False), ('Password', ''), ('SkipFirstLogin', True), ('TimeZone', 'UTC'), ('Street1', ''), ('Street2', ''), ('City', ''), ('State', ''), ('PostalCode', ''), ('Country', ''), ('CompanyName', ''), ('JobTitle', ''), ('CustomField1', ''), ('CustomField2', ''), ('CustomField4', ''), ('CustomField5', ''), ('CustomField6', ''), ('CustomField7', ''), ('CustomField8', ''), ('CustomField9', ''), ('CustomField10', ''), ('Culture', '')]))
-
+class TestLitmosType:
     def test_init(self):
-        user = User({'UserName': 'paul.smith', 'FirstName': 'Paul'})
+        user = LitmosType({'UserName': 'paul.smith', 'FirstName': 'Paul'})
 
-        assert_true(isinstance(user, User))
+        assert_true(isinstance(user, LitmosType))
         eq_(user.UserName, 'paul.smith')
         eq_(user.FirstName, 'Paul')
+
+    def test_name(self):
+        eq_(LitmosType.name(), 'litmostypes')
+
+    @patch('litmos.litmos.LitmosAPI')
+    def test_create(self, api_mock):
+        api_mock.create.return_value = {"Id": 'ws5tghd', "Name": "Paul"}
+        dummy_schema = OrderedDict([('Id', ''),('Name', '')])
+        LitmosType.SCHEMA = dummy_schema
+
+        lm = LitmosType.create({'Name': 'Paul'})
+
+        api_mock.create.assert_called_once_with('litmostypes', OrderedDict([('Id', ''), ('Name', 'Paul')]))
+        assert_true(isinstance(lm, LitmosType))
+        eq_(lm.Id, 'ws5tghd')
+        eq_(lm.Name, 'Paul')
+
+    @patch('litmos.litmos.LitmosAPI')
+    def test_find(self, api_mock):
+        api_mock.find.return_value = {"Id": 'ws5tghd', "Name": "Paul"}
+        dummy_schema = OrderedDict([('Id', ''),('Name', '')])
+        LitmosType.SCHEMA = dummy_schema
+
+        lm = LitmosType.find('ws5tghd')
+
+        api_mock.find.assert_called_once_with('litmostypes', 'ws5tghd')
+        assert_true(isinstance(lm, LitmosType))
+        eq_(lm.Id, 'ws5tghd')
+        eq_(lm.Name, 'Paul')
+
+    @patch('litmos.litmos.LitmosAPI')
+    def test_all(self, api_mock):
+        api_mock.all.return_value = [{"Id": 'ws5tghd', "Name": "Paul"}]
+        dummy_schema = OrderedDict([('Id', ''),('Name', '')])
+        LitmosType.SCHEMA = dummy_schema
+
+        lm_types = LitmosType.all()
+
+        api_mock.all.assert_called_once_with('litmostypes')
+        assert_true(isinstance(lm_types[0], LitmosType))
+        eq_(lm_types[0].Id, 'ws5tghd')
+        eq_(lm_types[0].Name, 'Paul')
+
+    @patch('litmos.litmos.LitmosAPI')
+    def test_search(self, api_mock):
+        api_mock.search.return_value = [{"Id": 'ws5tghd', "Name": "Paul"}]
+        dummy_schema = OrderedDict([('Id', ''),('Name', '')])
+        LitmosType.SCHEMA = dummy_schema
+
+        lm_types = LitmosType.search('Paul')
+
+        api_mock.search.assert_called_once_with('litmostypes', 'Paul')
+        assert_true(isinstance(lm_types[0], LitmosType))
+        eq_(lm_types[0].Id, 'ws5tghd')
+        eq_(lm_types[0].Name, 'Paul')
