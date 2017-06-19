d76f8637d07aacf8e9159d1032fbdc2c91489efd
Charlie Quinn <charlie.quinn@skyscanner.net>
2017-06-19 17:17:32 +0100

    Bump version: 0.3.0 → 0.4.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*names, **kwargs):
 
 setup(
     name='litmos-api',
-    version='0.3.0',
+    version='0.4.0',
     license='BSD',
     description='Python package integrating with Litmos User and Teams API',
     long_description='%s\n%s' % (
