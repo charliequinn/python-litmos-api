24b240f9d8a7ded16d1f2f760abd29373d769975
Charlie Quinn <charlie.quinn@skyscanner.net>
2019-10-25 16:07:48 +0100

    update python version

--- setup.py
+++ setup.py
@@ -47,6 +47,8 @@ setup(
         'Operating System :: Unix',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         # uncomment if you test on these interpreters:
